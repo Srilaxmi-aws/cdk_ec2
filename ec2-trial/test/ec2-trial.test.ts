ea25e5c2090179abeffb523ac8148b118bd8400e
Srilaxmi-aws <sri.awsgit@gmail.com>
2026-03-23 08:59:58 +0000

    ec2 code

---
+++ ec2-trial/test/ec2-trial.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2Trial from '../lib/ec2-trial-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2-trial-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2Trial.Ec2TrialStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
